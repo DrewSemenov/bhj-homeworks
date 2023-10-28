9b3582d495172599ce06f4e5292ccf37eadc381d
Andrei Semenov <drew.semenow@gmail.com>
2023-10-29 02:14:43 +0300

    one star completed

---
+++ element-search/menu/task.js
@@ -0,0 +1,17 @@
+let menuLink = [...document.querySelectorAll('.menu__link')].filter(
+  (el) => el.nextElementSibling
+);
+
+menuLink.forEach((el) => {
+  el.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    removeActive(menuLink);
+
+    event.target.nextElementSibling.classList.add('menu_active');
+  });
+});
+
+function removeActive(arr) {
+  arr.forEach((el) => el.nextElementSibling.classList.remove('menu_active'));
+}
