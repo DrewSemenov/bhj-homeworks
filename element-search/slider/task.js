f7615b336a0d4aed8bfea6c61ea2928fe205c831
Andrei Semenov <drew.semenow@gmail.com>
2023-10-29 04:00:56 +0300

    one star completed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,7 +1,11 @@
 const slides = [...document.querySelectorAll('.slider__item')];
+const dots = [...document.querySelectorAll('.slider__dot')];
 
 const arrowPrev = document.querySelector('.slider__arrow_prev');
 const arrowNext = document.querySelector('.slider__arrow_next');
+
+const classSliderActive = 'slider__item_active';
+const classDotActive = 'slider__dot_active';
 
 arrowNext.addEventListener('click', () => {
   showNextSlide(getCurrentSlideIndex());
@@ -11,23 +15,36 @@ arrowPrev.addEventListener('click', () => {
   showPrevSlide(getCurrentSlideIndex());
 });
 
+dots.forEach((el, index) => {
+  el.addEventListener('click', () => {
+    const currentIndex = getCurrentSlideIndex();
+
+    toggleAllElements(currentIndex, index);
+  });
+});
+
 function getCurrentSlideIndex() {
-  return slides.findIndex((el) => el.classList.contains('slider__item_active'));
+  return slides.findIndex((el) => el.classList.contains(classSliderActive));
 }
 
 function showNextSlide(index) {
   const nextIndex = index + 1 === slides.length ? 0 : index + 1;
 
-  toggleClass('slider__item_active', index, nextIndex);
+  toggleAllElements(index, nextIndex);
 }
 
 function showPrevSlide(index) {
   const prevIndex = index - 1 < 0 ? slides.length - 1 : index - 1;
 
-  toggleClass('slider__item_active', index, prevIndex);
+  toggleAllElements(index, prevIndex);
 }
 
-function toggleClass(className, currentIndex, nextIndex) {
-  slides[currentIndex].classList.remove(className);
-  slides[nextIndex].classList.add(className);
+function toggleAllElements(currentIndex, nextIndex) {
+  toggleClass(slides, classSliderActive, currentIndex, nextIndex);
+  toggleClass(dots, classDotActive, currentIndex, nextIndex);
+}
+
+function toggleClass(arr, className, currentIndex, nextIndex) {
+  arr[currentIndex].classList.remove(className);
+  arr[nextIndex].classList.add(className);
 }
