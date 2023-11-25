3bd09257f7c0bb811eb95e95ba5d63efad09f12c
Andrei Semenov <drew.semenow@gmail.com>
2023-11-25 23:58:48 +0300

    all star completed

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -3,22 +3,61 @@ class ListOfInterests {
     this.main = document.querySelector('.interests_main');
     this.itemsArray = document.querySelectorAll('.interest');
 
-    this.main.addEventListener('click', this.checkFull.bind(this));
+    this.main.addEventListener('click', this.checkItems.bind(this));
   }
 
-  checkFull(event) {
+  checkItems(event) {
     const targetItem = event.target.closest('input');
 
     if (!targetItem) {
       return;
     }
 
-    const targetParent = targetItem.closest('ul');
-    const nextElement = targetItem.closest('li');
-    const nestedElements = [
-      ...nextElement.querySelectorAll(':scope > ul input'),
+    this.checkNested(targetItem);
+    this.checkParent(targetItem);
+  }
+
+  checkNested(element) {
+    const nextElement = element.closest('li');
+
+    const nestedElements = [...nextElement.querySelectorAll('ul input')];
+    nestedElements.forEach((el) => {
+      el.checked = element.checked;
+      el.indeterminate = false;
+    });
+  }
+
+  checkParent(element) {
+    const parent = element.closest('ul').closest('li');
+    if (!parent) {
+      return;
+    }
+
+    const parentInput = parent.querySelector('input');
+
+    const childItems = [
+      ...parent.querySelectorAll(':scope ul > li > label > input'),
     ];
-    nestedElements.forEach((el) => (el.checked = targetItem.checked));
+
+    const childItemsLength = childItems.length;
+    const checkedChild = childItems.filter((el) => el.checked).length;
+
+    if (!checkedChild) {
+      parentInput.checked = false;
+      parentInput.indeterminate = false;
+    }
+
+    if (checkedChild && checkedChild !== childItemsLength) {
+      parentInput.checked = false;
+      parentInput.indeterminate = true;
+    }
+
+    if (checkedChild === childItemsLength) {
+      parentInput.checked = true;
+      parentInput.indeterminate = false;
+    }
+
+    this.checkParent(parentInput);
   }
 }
 
