372d091edf97d4bbfef2f4988684dcde4c9a393e
Andrei Semenov <drew.semenow@gmail.com>
2023-11-26 09:04:54 +0300

    one star && scroll completed

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,63 @@
+class Tooltip {
+  constructor() {
+    this.body = document.querySelector('body');
+
+    this.createTooltip();
+    this.registerListeners();
+  }
+
+  createTooltip() {
+    this.tooltipClassActive = 'tooltip_active';
+    this.tooltip = document.createElement('div');
+    this.tooltip.classList.add('tooltip');
+    this.tooltip.style.cursor = 'pointer';
+  }
+
+  registerListeners() {
+    this.body.addEventListener('click', (event) => this.showTooltip(event));
+    this.tooltip.addEventListener('click', () => this.hideTooltip());
+    window.addEventListener('scroll', () => this.scrollPositionTooltip());
+  }
+
+  showTooltip(event) {
+    const linkHasTooltip = event.target.closest('.has-tooltip');
+
+    if (!linkHasTooltip) {
+      return;
+    }
+
+    event.preventDefault();
+
+    this.addTooltip(linkHasTooltip);
+  }
+
+  addTooltip(element) {
+    this.tooltip.textContent = element.title;
+    this.tooltip.classList.add(this.tooltipClassActive);
+
+    this.getPositionTooltip(element);
+
+    element.after(this.tooltip);
+  }
+
+  getPositionTooltip(element) {
+    const elementPosition = element.getBoundingClientRect();
+
+    this.tooltip.style.left = elementPosition.left + 'px';
+    this.tooltip.style.top = elementPosition.bottom + 'px';
+  }
+
+  hideTooltip() {
+    this.tooltip.classList.remove(this.tooltipClassActive);
+  }
+
+  scrollPositionTooltip() {
+    if (!this.tooltip.classList.contains(this.tooltipClassActive)) {
+      return;
+    }
+
+    this.getPositionTooltip(this.tooltip.previousElementSibling);
+  }
+}
+
+const tooltip = new Tooltip();
