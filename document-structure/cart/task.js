c7d71b1180af74274939bfb362b85ca4068299df
Andrei Semenov <drew.semenow@gmail.com>
2023-12-03 07:39:55 +0300

    add check product incart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,6 +1,8 @@
 class Cart {
-  cart = document.querySelector('.cart');
+  cartProducts = document.querySelector('.cart__products');
   products = document.querySelector('.products');
+
+  productsArray = [];
 
   constructor() {
     this.registerEvent();
@@ -11,19 +13,27 @@ class Cart {
   }
 
   clickEvent(event) {
-    const target = event.target.closest('.product__quantity-control');
+    const controlsButton = event.target.closest('.product__quantity-control');
+    if (controlsButton) {
+      this.controlsClick(controlsButton);
+    }
 
-    if (!target) return;
+    const addButton = event.target.closest('.product__add');
+    if (addButton) {
+      this.addToCart(addButton.closest('.product'));
+    }
+  }
 
-    const valueElement = target
+  controlsClick(elem) {
+    const valueElement = elem
       .closest('.product__quantity-controls')
       .querySelector('.product__quantity-value');
 
-    if (target.classList.contains('product__quantity-control_inc')) {
+    if (elem.classList.contains('product__quantity-control_inc')) {
       this.controlInc(valueElement);
     }
 
-    if (target.classList.contains('product__quantity-control_dec')) {
+    if (elem.classList.contains('product__quantity-control_dec')) {
       this.controlDec(valueElement);
     }
   }
@@ -35,6 +45,41 @@ class Cart {
   controlDec(elem) {
     if (elem.textContent < 2) return;
     elem.textContent--;
+  }
+
+  addToCart(product) {
+    let productCount = product.querySelector('.product__quantity-value');
+
+    const productObj = {
+      id: product.dataset.id,
+      src: product.querySelector('.product__image').src,
+      count: productCount.textContent,
+    };
+
+    const productInCart = this.cartProducts.querySelector(
+      `[data-id="${productObj.id}"]`
+    );
+
+    if (productInCart) {
+      const countElement = productInCart.querySelector('.cart__product-count');
+      countElement.textContent =
+        Number(countElement.textContent) + Number(productObj.count);
+    } else {
+      this.productsArray.push(productObj);
+      const productElement = this.getProductHtml(productObj);
+      this.cartProducts.insertAdjacentHTML('beforeend', productElement);
+    }
+
+    productCount.textContent = 1;
+  }
+
+  getProductHtml({ id, src, count }) {
+    return `
+    <div class="cart__product" data-id=${id}>
+      <img class="cart__product-image" src=${src}>
+      <div class="cart__product-count">${count}</div>
+    </div>
+    `;
   }
 }
 
